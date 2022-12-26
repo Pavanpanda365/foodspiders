c70550c7de872cb9129102a0e75392926ebe9f26
Pavanpanda365 <pavanpanda365@gmail.com>
2022-12-26 12:57:00 +0530

    foodspiders App

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import food from '../food.png'
+
+const Navbar = () => {
+	return ( 
+		<nav>
+			<div className="logo">
+				<img src={food} alt="logo" />
+				<h1> Food Spider</h1>
+			</div>
+			
+			<div className="navlinks">
+				<a href="/">Add food</a>
+				<a href="/">Orders</a>
+			</div>
+
+		</nav>
+	 );
+}
+ 
+export default  Navbar;
